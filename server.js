c8432ef7a212dffb2607688e4dc86e6108c40d98
maiamona <78083077+maiamona@users.noreply.github.com>
2021-05-20 09:52:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const app = express();
 
 app.get('/', (req, res)=>{
     res.status(200).send({message: 'olá mundo estou funcionando'})
-})
+});
+
+app.get('/ola', (req, res)=>{
+    res.status(200).send({message: 'oi eu sou a rota ola'})
+});
 
 app.listen(3001, ()=>{
     console.log('Api rodando na porta 3001');
